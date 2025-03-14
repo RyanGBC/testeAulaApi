9f8555524c65f927e0b90f0c4bee30e218fd130d
RyanGBC <ryangabrj@gmail.com>
2025-03-14 11:04:12 -0300

    feat: arrumando o codigo

---
+++ testeAulaApi/main.py
@@ -0,0 +1,15 @@
+import requests
+
+reqUrl = "https://api.github.com/octocat"
+
+headerList = {
+    "Accept": "aplication/vdn.github+json",
+    "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+    "X-GitHub-Api-Version": "2022-11-28"
+}
+
+payload = ""
+
+response = requests.request("GET", reqUrl, data=payload, headers=headerList)
+
+print(response.text)
