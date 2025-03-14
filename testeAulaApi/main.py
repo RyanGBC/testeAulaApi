1ae816213cdb9fb33087b6c6a08c128d910cd332
RyanGBC <ryangabrj@gmail.com>
2025-03-14 11:09:37 -0300

    feat: codigo correto da API do github

--- testeAulaApi/main.py
+++ testeAulaApi/main.py
@@ -1,15 +1,12 @@
 import requests
 
 reqUrl = "https://api.github.com/octocat"
-
 headersList = {
-    "Accept": "aplication/vdn.github+json",
+    "Accept": "application/vnd.github+json",
     "User-Agent": "Thunder Client (https://www.thunderclient.com)",
     "X-GitHub-Api-Version": "2022-11-28"
 }
 
 payload = ""
-
 response = requests.request("GET", reqUrl, data=payload, headers=headersList)
-
 print(response.text)
